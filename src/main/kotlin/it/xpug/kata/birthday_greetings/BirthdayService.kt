6b5b9dfabbe4e313056740f85ef8adc3b0372a3c
Timoty Granziero <timoty.granziero@gmail.com>
2020-07-02 12:32:27 +0200

    Refactor

--- src/main/kotlin/it/xpug/kata/birthday_greetings/BirthdayService.kt
+++ src/main/kotlin/it/xpug/kata/birthday_greetings/BirthdayService.kt
@@ -6,16 +6,18 @@ import java.io.FileReader
 class BirthdayService(val notificationService: NotificationService) {
     fun sendGreetings(fileName: String?, xDate: XDate) {
         val reader = BufferedReader(FileReader(fileName))
-        reader.readLine()
-        reader.readLines().forEach {
-            val employeeData = it.split(", ")
-            val employee = Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3])
-            if (employee.isBirthday(xDate)) {
-                val recipient = employee.email
-                val body = "Happy Birthday, dear %NAME%".replace("%NAME%", employee.firstName!!)
-                val subject = "Happy Birthday!"
-                notificationService.sendMessage("sender@here.com", subject, body, recipient)
+        val lines = reader.readLines()
+        lines
+            .takeLast(lines.size - 1)
+            .forEach {
+                val employeeData = it.split(", ")
+                val employee = Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3])
+                if (employee.isBirthday(xDate)) {
+                    val recipient = employee.email
+                    val body = "Happy Birthday, dear %NAME%".replace("%NAME%", employee.firstName!!)
+                    val subject = "Happy Birthday!"
+                    notificationService.sendMessage("sender@here.com", subject, body, recipient)
+                }
             }
-        }
     }
 }
