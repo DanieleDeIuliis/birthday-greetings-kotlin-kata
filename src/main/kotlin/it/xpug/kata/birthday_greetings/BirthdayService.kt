5b366103247bc6d6704c2077ba00427848803be7
DanieleDeIuliis <daniele.deiuliis@gmail.com>
2021-03-08 10:29:13 +0100

    first small extraction of methods in birthdayService

--- src/main/kotlin/it/xpug/kata/birthday_greetings/BirthdayService.kt
+++ src/main/kotlin/it/xpug/kata/birthday_greetings/BirthdayService.kt
@@ -15,15 +15,19 @@ class BirthdayService {
         var str : String? = ""
         str = reader.readLine() // skip header
         while (reader.readLine().also { str = it } != null) {
-            val employeeData = str!!.split(", ")
-            val employee = Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3])
+            val employee = parseEmployee(str)
             if (employee.isBirthday(xDate)) {
                 val recipient = employee.email
-                val body = "Happy Birthday, dear %NAME%".replace("%NAME%", employee.firstName!!)
+                val body = "Happy Birthday, dear ${employee.firstName}!"
                 val subject = "Happy Birthday!"
                 sendMessage(smtpHost, smtpPort, "sender@here.com", subject, body, recipient)
             }
         }
+    }
+
+    private fun parseEmployee(str: String?): Employee {
+        val employeeData = str!!.split(", ")
+        return Employee(employeeData[1], employeeData[0], employeeData[2], employeeData[3])
     }
 
     private fun sendMessage(
@@ -34,20 +38,33 @@ class BirthdayService {
         body: String,
         recipient: String?
     ) {
-        // Create a mail session
-        val props = Properties()
-        props["mail.smtp.host"] = smtpHost
-        props["mail.smtp.port"] = "" + smtpPort
-        val session = Session.getInstance(props, null)
+        val session = createSession(smtpHost, smtpPort)
 
-        // Construct the message
+        val msg: Message = buildMessage(session, sender, recipient, subject, body)
+
+        Transport.send(msg)
+    }
+
+    private fun buildMessage(
+        session: Session?,
+        sender: String,
+        recipient: String?,
+        subject: String,
+        body: String
+    ): Message {
         val msg: Message = MimeMessage(session)
         msg.setFrom(InternetAddress(sender))
         msg.setRecipient(Message.RecipientType.TO, InternetAddress(recipient))
         msg.subject = subject
         msg.setText(body)
+        return msg
+    }
 
-        // Send the message
-        Transport.send(msg)
+    private fun createSession(smtpHost: String, smtpPort: Int): Session? {
+        val props = Properties()
+        props["mail.smtp.host"] = smtpHost
+        props["mail.smtp.port"] = "" + smtpPort
+        val session = Session.getInstance(props, null)
+        return session
     }
 }
